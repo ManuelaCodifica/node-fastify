0e8758d578339483bbf89ecb571ac6ecf4b11951
Manuela Alecio <manuelaalecio.dev@gmail.com>
2025-04-04 15:54:41 -0300

    feat: cofing envs

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,15 @@
 import fastify from "fastify";
 import { knex } from "./database";
+import { env } from "./env";
 
 const app = fastify();
 
 app.get("/", async () => {
-const tables = await knex("sqlite_schema").select("*");
+  const transactions = await knex("transactions").select("*");
 
-return tables
+  return transactions;
 });
 
-app.listen({ port: 3333 }).then(() => {
-  console.log("Server is running on http://localhost:3333");
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`Server is running on http://localhost:${env.PORT}`);
 });
